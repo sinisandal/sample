688f03c189ef3ac35ddc002ec3fc2c9f606d09cb
Training_B4C.05.15 <Training_B4C.05.15@BDC7-D-1SR6N62.dir.svc.accenture.com>
2019-08-13 17:05:32 +0530

    coomit1

---
+++ JavaDemo/src/pack1/Calculator.java
@@ -0,0 +1,11 @@
+package pack1;
+
+public class Calculator {
+	public static void main(String[] args) {
+		int v1=10;
+		int v2=20;
+		int res=v1+v2;
+		System.out.println("Total mark is:"+res);
+	}
+
+}
